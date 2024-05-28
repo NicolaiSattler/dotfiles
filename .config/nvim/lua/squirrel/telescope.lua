763022960cf9431e45f69b8a12ec22c47f38ed03
Nicolai Sattler <nicolaisattler1990@gmail.com>
2024-05-28 15:21:47 +0200

    configure fzf for telescope

--- .config/nvim/lua/squirrel/telescope.lua
+++ .config/nvim/lua/squirrel/telescope.lua
@@ -7,6 +7,14 @@ require('telescope').setup {
       },
     },
   },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case"
+    }
+  }
 }
 
 -- Enable telescope fzf native, if installed
