e44cf186954ad35786b5e2e0c21ae4f7d819e774
Nicolai Sattler <nicolaisattler1990@gmail.com>
2025-07-30 10:27:17 +0200

    fixed cmp icons + cmp lsp issue

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "rose-pine"
-config.font = wezterm.font("Hack Nerd Font", { weight = 'Bold' })
+config.font = "Fire Code"
 config.font_size = 10
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
