99dc11a55cd8c0a08927d7814cceae77448c7ebb
Nicolai Sattler <nicolaisattler1990@gmail.com>
2025-08-18 20:42:54 +0200

    tweak cmp coloring

--- .config/nvim/lua/squirrel/cmp.lua
+++ .config/nvim/lua/squirrel/cmp.lua
@@ -40,17 +40,37 @@ local kind_icons = {
   TypeParameter = "󰅲",
   Copilot = ""
 }
+-- gray
+vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg='NONE', strikethrough=true, fg='#808080' })
+-- blue
+vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg='NONE', fg='#569CD6' })
+vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link='CmpIntemAbbrMatch' })
+-- light blue
+vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg='NONE', fg='#9CDCFE' })
+vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link='CmpItemKindVariable' })
+vim.api.nvim_set_hl(0, 'CmpItemKindText', { link='CmpItemKindVariable' })
+-- pink
+vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg='NONE', fg='#C586C0' })
+vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link='CmpItemKindFunction' })
+-- front
+vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg='NONE', fg='#D4D4D4' })
+vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link='CmpItemKindKeyword' })
+vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link='CmpItemKindKeyword' })
 
 cmp.setup {
   formatting = {
     format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
-      maxwidth = 50,
+      mode = 'symbol_text',
+      maxwidth = 80,
       symbol_map = kind_icons,
       ellipsis_char = '...',
       show_labelDetails = true,
     })
   },
+  view = {
+    entries = { name = 'custom', selection_order = 'near_cursor' }
+  },
+
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
@@ -103,7 +123,6 @@ cmp.setup {
     { name = 'render-markdown' }
   },
 }
-
 cmp.setup.cmdline('/', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = {
