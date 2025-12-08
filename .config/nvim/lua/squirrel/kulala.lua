78b05a39774f86bcb75513d09b495b0fb81f23d0
Nicolai Sattler <nicolaisattler1990@gmail.com>
2025-12-08 08:50:53 +0100

    start configuring lazyload plugins to decrease startup time

--- .config/nvim/lua/squirrel/kulala.lua
+++ .config/nvim/lua/squirrel/kulala.lua
@@ -1,10 +1,17 @@
-local kulala = require("kulala")
+return {
+  "mistweaverco/kulala.nvim",
+  lazy = true,
+  ft = { "http", "rest" },
+  config = function()
+    local kulala = require("kulala.config")
 
-kulala.setup({
-	global_keymaps = true,
-	global_keymaps_prefix = "<leader>K",
-	kulala_keymaps_prefix = "",
-	opts = {
-		request_timeout = 10000,
-	},
-})
+    kulala.setup({
+      global_keymaps = true,
+      global_keymaps_prefix = "<leader>K",
+      kulala_keymaps_prefix = "",
+      opts = {
+        request_timeout = 10000,
+      },
+    })
+  end,
+}
--- .config/nvim/lua/squirrel/lazy.lua
+++ .config/nvim/lua/squirrel/lazy.lua
@@ -1,207 +1,203 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	--git
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "InsertEnter",
-	},
+  --git
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "InsertEnter",
+  },
 
-	-- autodetect tabstop and shiftwidth
-	{
-		"tpope/vim-sleuth",
-		event = "InsertEnter",
-	},
+  -- autodetect tabstop and shiftwidth
+  {
+    "tpope/vim-sleuth",
+    event = "InsertEnter",
+  },
 
-	-- lsp
-	{
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			},
-		},
-	},
-	{
-		-- Main LSP Configuration
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			-- Automatically install LSPs and related tools to stdpath for Neovim
-			-- Mason must be loaded before its dependents so we need to set it up here.
-			-- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-			{ "mason-org/mason.nvim", opts = {} },
-			"mason-org/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
+  -- lsp
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",
+    opts = {
+      library = {
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  {
+    -- Main LSP Configuration
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- Automatically install LSPs and related tools to stdpath for Neovim
+      -- Mason must be loaded before its dependents so we need to set it up here.
+      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
+      { "mason-org/mason.nvim", opts = {} },
+      "mason-org/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
 
-			-- Useful status updates for LSP.
-			{ "j-hui/fidget.nvim", opts = {} },
+      -- Useful status updates for LSP.
+      { "j-hui/fidget.nvim", opts = {} },
 
-			-- Allows extra capabilities provided by blink.cmp
-			"saghen/blink.cmp",
-		},
-	},
-	--dotnet <3
-	{
-		"GustavEikaas/easy-dotnet.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-	},
-	-- start window
-	"folke/twilight.nvim",
+      -- Allows extra capabilities provided by blink.cmp
+      "saghen/blink.cmp",
+    },
+  },
+  --dotnet <3
+  {
+    "GustavEikaas/easy-dotnet.nvim",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+  -- start window
+  "folke/twilight.nvim",
 
-	-- noice - pretty commands :o
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {},
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-	},
-	--better quickfixlist
-	{
-		"stevearc/quicker.nvim",
-		ft = "qf",
-		---@module "quicker"
-		---@type quicker.SetupOptions
-		opts = {},
-	},
-	-- workspace diagnosics
-	{
-		"artemave/workspace-diagnostics.nvim",
-	},
-	--debugger
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-			"leoluz/nvim-dap-go",
-		},
-	},
+  -- noice - pretty commands :o
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+  --better quickfixlist
+  {
+    "stevearc/quicker.nvim",
+    ft = "qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+  },
+  -- workspace diagnosics
+  {
+    "artemave/workspace-diagnostics.nvim",
+  },
+  --debugger
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+      "leoluz/nvim-dap-go",
+    },
+  },
 
-	-- Autocompletion
-	require("squirrel.blink-cmp"),
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				auth_provider_url = "https://stichting-bkr.ghe.com",
-				suggestion = { enabled = false },
-				panel = { enabled = false },
-			})
-		end,
-	},
+  -- Autocompletion
+  require("squirrel.blink-cmp"),
+  require("squirrel.copilot"),
 
-	-- file explorer
-	{
-		"stevearc/oil.nvim",
-		opts = {},
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
-	-- Undotree
-	{
-		"jiaoshijie/undotree",
-		dependencies = "nvim-lua/plenary.nvim",
-	},
+  -- file explorer
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  -- Undotree
+  {
+    "jiaoshijie/undotree",
+    lazy = true,
+    dependencies = "nvim-lua/plenary.nvim",
+  },
 
-	-- REST client
-	{
-		"mistweaverco/kulala.nvim",
-		ft = "http",
-	},
+  -- REST client
+  require("squirrel.kulala"),
 
-	-- Useful plugin to show you pending keybinds.
-	{ "folke/which-key.nvim", opts = {} },
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("tokyonight").setup({
-				styles = {
-					comments = { italic = false }, -- Disable italics in comments
-				},
-			})
-		end,
-	},
+  -- Useful plugin to show you pending keybinds.
+  {
+    "folke/which-key.nvim",
+    lazy = true,
+    opts = {},
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("tokyonight").setup({
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      })
+    end,
+  },
 
-	"nvim-lualine/lualine.nvim",
-	{
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		-- Add indentation guides even on blank lines
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
-	},
-	{
-		"epwalsh/obsidian.nvim",
-		version = "*",
-		lazy = true,
-		ft = "markdown",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-		opts = {},
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-lua/popup.nvim",
-				"nvim-lua/plenary.nvim",
-				"nvim-telescope/telescope-fzf-native.nvim",
-				"nvim-telescope/telescope-media-files.nvim",
-				build = "make",
-				cond = function()
-					return vim.fn.executable("make") == 1
-				end,
-			},
-		},
-	},
-	-- "gc" to comment visual regions/lines
-	{
-		"numToStr/Comment.nvim",
-		opts = {},
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-	},
-	-- autoformatter
-	require("squirrel.conform"),
+  "nvim-lualine/lualine.nvim",
+  {
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    -- Add indentation guides even on blank lines
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    lazy = true,
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-lua/popup.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-fzf-native.nvim",
+        "nvim-telescope/telescope-media-files.nvim",
+        build = "make",
+        cond = function()
+          return vim.fn.executable("make") == 1
+        end,
+      },
+    },
+  },
+  -- "gc" to comment visual regions/lines
+  {
+    "numToStr/Comment.nvim",
+    opts = {},
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+  },
+  -- autoformatter
+  require("squirrel.conform"),
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		build = ":TSUpdate",
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+  },
 }, {})
