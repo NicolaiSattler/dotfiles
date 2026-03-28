1f1466f9676b4a79b568b1ffa9f79bc870efd767
Nicolai Sattler <nicolaisattler1990@gmail.com>
2026-03-28 22:05:51 +0100

    use tokyonight day + borderless telescope

--- .config/nvim/lua/squirrel/colors.lua
+++ .config/nvim/lua/squirrel/colors.lua
@@ -1,4 +1,4 @@
 -- vim.cmd.colorscheme 'rose-pine'
-vim.cmd.colorscheme("tokyonight-night")
+vim.cmd.colorscheme("tokyonight-day")
 -- Set background to transparent
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
--- .config/nvim/lua/squirrel/lazy/tokyonight.lua
+++ .config/nvim/lua/squirrel/lazy/tokyonight.lua
@@ -4,9 +4,36 @@ return {
   priority = 1000,
   config = function()
     require("tokyonight").setup({
-      styles = {
-        comments = { italic = false },
-      },
+      on_highlights = function(hl, c)
+        local prompt = "#d0d5e3"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
     })
   end,
 }
