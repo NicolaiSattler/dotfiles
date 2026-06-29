e738ec53c579a265430c9944c944885590e08e4e
Nicolai Sattler <nicolaisattler1990@gmail.com>
2026-06-29 10:48:01 +0200

    fix obsidian config

--- .config/nvim/lua/squirrel/lazy/obsidian.lua
+++ .config/nvim/lua/squirrel/lazy/obsidian.lua
@@ -1,17 +1,24 @@
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*",
   lazy = true,
   ft = "markdown",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  opt = {
+  opts = {
+    legacy_commands = false,
+    sync = {
+      enabled = true,
+    },
     workspaces = {
       {
         name = "default",
         path = "/mnt/d/iCloudDrive/iCloud~md~obsidian/Vault",
       },
+    },
+    daily_notes = {
+      folder = "Work/Dail Notes",
+      date_format = "DD-MM-YYYY",
+      default_tags = { "daily-notes" },
+      workdays_only = true,
     },
   },
 }
