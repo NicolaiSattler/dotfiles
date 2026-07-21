800a4f95f5d87899be252260efa0e5e99bcc8752
Nicolai Sattler <nicolaisattler1990@gmail.com>
2026-07-21 16:20:37 +0200

    fix Bda command

--- .config/nvim/lua/squirrel/commands.lua
+++ .config/nvim/lua/squirrel/commands.lua
@@ -1,14 +1,14 @@
 -- close all buffers except the focused buffer one
 vim.api.nvim_create_user_command("Bda", function()
-	local current = vim.api.nvim_get_current_buf()
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if vim.api.nvim_buf_is_loaded(buf) and buf ~= current then
-			vim.api.nvim_buf_delete(buf, {})
-		end
-	end
+  local current = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if buf ~= current and vim.fn.buflisted(buf) == 1 then
+      vim.api.nvim_buf_delete(buf, {})
+    end
+  end
 end, {})
 
 vim.api.nvim_create_user_command("Code", function()
-	vim.notify("Opened VSCode in current directory", vim.log.levels.INFO)
-	vim.fn.jobstart({ "code", "." }, { detach = true })
+  vim.notify("Opened VSCode in current directory", vim.log.levels.INFO)
+  vim.fn.jobstart({ "code", "." }, { detach = true })
 end, {})
